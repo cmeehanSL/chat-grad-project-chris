19ee808bae960c5fad658efd7f830fa3c966d6ba
Steven Hillcox <shillcox@scottlogic.co.uk>
2016-08-12 08:06:37 +0100

    Setup

---
+++ server.js
@@ -0,0 +1,18 @@
+var server = require("./server/server");
+var oAuthGithub = require("./server/oauth-github");
+var MongoClient = require("mongodb").MongoClient;
+
+var port = process.env.PORT || 8080;
+var dbUri = process.env.DB_URI || "mongodb://test:test@ds027491.mongolab.com:27491/chat-grad-project";
+var oauthClientId = process.env.OAUTH_CLIENT_ID || "fa4a22095c46dfc1d832";
+var oauthSecret = process.env.OAUTH_SECRET || "4bbf1b48173c3cbc35917fad9f94ef2b584cbaa4";
+
+MongoClient.connect(dbUri, function(err, db) {
+    if (err) {
+        console.log("Failed to connect to db", err);
+        return;
+    }
+    var githubAuthoriser = oAuthGithub(oauthClientId, oauthSecret);
+    server(port, db, githubAuthoriser);
+    console.log("Server running on port " + port);
+});
---
+++ server/server.js
@@ -0,0 +1,90 @@
+var express = require("express");
+var cookieParser = require("cookie-parser");
+
+module.exports = function(port, db, githubAuthoriser) {
+    var app = express();
+
+    app.use(express.static("public"));
+    app.use(cookieParser());
+
+    var users = db.collection("users");
+    var sessions = {};
+
+    app.get("/oauth", function(req, res) {
+        githubAuthoriser.authorise(req, function(githubUser, token) {
+            if (githubUser) {
+                users.findOne({
+                    _id: githubUser.login
+                }, function(err, user) {
+                    if (!user) {
+                        // TODO: Wait for this operation to complete
+                        users.insertOne({
+                            _id: githubUser.login,
+                            name: githubUser.name,
+                            avatarUrl: githubUser.avatar_url
+                        });
+                    }
+                    sessions[token] = {
+                        user: githubUser.login
+                    };
+                    res.cookie("sessionToken", token);
+                    res.header("Location", "/");
+                    res.sendStatus(302);
+                });
+            }
+            else {
+                res.sendStatus(400);
+            }
+
+        });
+    });
+
+    app.get("/api/oauth/uri", function(req, res) {
+        res.json({
+            uri: githubAuthoriser.oAuthUri
+        });
+    });
+
+    app.use(function(req, res, next) {
+        if (req.cookies.sessionToken) {
+            req.session = sessions[req.cookies.sessionToken];
+            if (req.session) {
+                next();
+            } else {
+                res.sendStatus(401);
+            }
+        } else {
+            res.sendStatus(401);
+        }
+    });
+
+    app.get("/api/user", function(req, res) {
+        users.findOne({
+            _id: req.session.user
+        }, function(err, user) {
+            if (!err) {
+                res.json(user);
+            } else {
+                res.sendStatus(500);
+            }
+        });
+    });
+
+    app.get("/api/users", function(req, res) {
+        users.find().toArray(function(err, docs) {
+            if (!err) {
+                res.json(docs.map(function(user) {
+                    return {
+                        id: user._id,
+                        name: user.name,
+                        avatarUrl: user.avatarUrl
+                    };
+                }));
+            } else {
+                res.sendStatus(500);
+            }
+        });
+    });
+
+    return app.listen(port);
+};
